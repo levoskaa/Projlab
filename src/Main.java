4fe025b356efdcb3ef6407a9d0079f828f4529f9
stateman92 <teglatest202@gmail.com>
2019-02-09 23:15:59 +0100

    probauzenet2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("LEVOSKAA!");
         System.out.println("panda");
+        System.out.println("második panda");
     }
 }

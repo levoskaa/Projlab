2b294ae9073f05678137e44cf19f99797cc55029
Laurinyecz Levente <47329402+levoskaa@users.noreply.github.com>
2019-02-21 18:14:24 +0100

    Create Class diagram

---
+++ Solution/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("LEVOSKAA!");
+        System.out.println("panda");
+        System.out.println("második panda");
+        System.out.println("harmadik");
+    }
+}

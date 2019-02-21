d810fda2818dfac419b06e3c252389723920fc1d
Laurinyecz Levente <47329402+levoskaa@users.noreply.github.com>
2019-02-21 18:20:15 +0100

    "Finalize removes"

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("LEVOSKAA!");
-        System.out.println("panda");
-        System.out.println("második panda");
-        System.out.println("harmadik");
-    }
-}

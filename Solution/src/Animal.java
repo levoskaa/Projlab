aa7856a378cf5809d760354be5654899bdc5677c
Laurinyecz Levente <47329402+levoskaa@users.noreply.github.com>
2019-03-20 15:47:12 +0100

    Create java code from UML diagrams

---
+++ Solution/src/Animal.java
@@ -0,0 +1,53 @@
+//  @ Project : Projlab
+//  @ File Name : Animal.java
+//  @ Date : 2019.03.20.
+//  @ Author : Laurinyecz
+
+/**
+ * Osztaly az allatok viselkedesenek es tulajdonsagaiknak osszegyujtesere,
+ * a Panda es Orangutan osztalyok ebbol az osztalybol szarmaznak.
+ * @author Laurinyecz
+ * @see Panda
+ * @see Orangutan
+ */
+public abstract class Animal {
+    /**
+     * Arra a csempere mutat, amelyiken az allat eppen all.
+     */
+    private BaseTile currentTile;
+
+    /**
+     * Getter fuggveny a csempe lekerdezesere.
+     * @return Csempe(BaseTile objektum) amelyiken az allat eppen all.
+     */
+    public BaseTile getTile() {
+        return currentTile;
+    }
+
+    /**
+     * Setter fuggveny a csempe beallitasara.
+     * @param t Az uj csempe, amelyikre athelyezzuk az allatot.
+     */
+    public void setTile(BaseTile t) {
+        currentTile = t;
+    }
+
+    public abstract void move();
+
+    public abstract void sit(Tile t);
+    
+    public abstract void collision(Animal a);
+    
+    public abstract void scare();
+    
+    public abstract void jump();
+    
+    public abstract void checkPoints();
+    
+    public abstract void catchPanda();
+    
+    public void die() {
+    }
+    
+    public abstract void delegateTick();
+}
---
+++ Solution/src/Panda.java
@@ -0,0 +1,24 @@
+//  @ Project : Projlab
+//  @ File Name : Panda.java
+//  @ Date : 2019.03.20.
+//  @ Author : 
+//
+//
+
+
+
+
+public abstract class Panda extends Animal implements IAnimal, InteractiveThings {
+    public void checkPoints() {
+    }
+    
+    public void setCaught() {
+    }
+    
+    public void die() {
+    }
+    
+    public void delegateTick();
+    
+    public void doYourThing();
+}
